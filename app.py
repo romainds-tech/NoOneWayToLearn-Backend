cd844d1154ef27b8b0c24467574c3dbad8c18f6b
Romain Da Silva <38rom1@gmail.com>
2024-01-10 01:00:03 +0100

    update api

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 import datetime
 
+from api.no_one_way_to_learn.ai.no_one_way_to_learn_model import (
+    create_model_ml,
+    normalize_input,
+)
+from api.no_one_way_to_learn.ai.predict import predict_nowtl, process
+from api.no_one_way_to_learn.user.schemas import UserSchema
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from api.no_one_way_to_learn.user.schemas import UserSchema
-from api.no_one_way_to_learn.ai.no_one_way_to_learn_model import create_model_ml
-from api.no_one_way_to_learn.ai.predict import predict_nowtl
-
 from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
 
 limiter = Limiter(key_func=get_remote_address)
 
 app = FastAPI(docs_url=None, redoc_url="/doc")
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*']
-)
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+
+load_dotenv()
 
 
 @app.post("/user", response_model=UserSchema, status_code=201)
@@ -31,32 +34,35 @@ async def index(request: Request, schema: UserSchema):
 @limiter.limit("2/minute")
 async def create_model(request: Request):
     create_model_ml()
-    return {"message": "Model created successfully at : " + str(datetime.datetime.now())}
+    return {
+        "message": "Model created successfully at : " + str(datetime.datetime.now())
+    }
 
 
 @app.get("/predict", status_code=200)
 @limiter.limit("120/minute")
-async def predict(request: Request):
-    equivalent = {
-        "never": "1",
-        "sometimes": "2",
-        "occasionally": "3",
-        "lot": "4"
-    }
+async def predict(request: Request, age, cursus, side_project, open_source):
+    equivalent = {"never": "1", "sometimes": "2", "occasionally": "3", "lot": "4"}
 
-    equi_cursus = {
-        "tech": "1",
-        "engineer": "2"
-    }
+    equi_cursus = {"tech": "1", "engineer": "2"}
 
-    parameters = request.query_params
+    normalized_inputs = normalize_input(
+        [age, equi_cursus[cursus], equivalent[side_project], equivalent[open_source]],
+        12,
+        99,
+    )
 
-    print(parameters.items())
-
-    age, cursus, side_project, open_source = parameters.items()
-
-    res = predict_nowtl(age[1], equi_cursus[cursus[1]], equivalent[side_project[1]], equivalent[open_source[1]])
+    res = predict_nowtl(*normalized_inputs)
 
     print("res: ", res)
 
     return "Prediction successfully.", res.flatten().tolist()
+
+
+@app.get("/generate_exercices", status_code=200)
+@limiter.limit("2/minute")
+async def generate_exercices(
+    request: Request, cursus: str = "", exp: str = "", appinf: str = "", temoi: str = ""
+):
+    detail = await process(cursus, exp, appinf, temoi)
+    return detail
